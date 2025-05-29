9557cf80b8c50b8ed75b66d30081bd948e4f8665
Yevhenii <evgeniy.slobod9@gmail.com>
2025-05-29 13:36:59 +0300

    Create a devflow application

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+				<h1 className="text-4xl">Welcome to course</h1>
         {children}
       </body>
     </html>
